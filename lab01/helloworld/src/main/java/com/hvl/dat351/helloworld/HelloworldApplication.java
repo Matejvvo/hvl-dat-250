fa37eaa93db50887c6416bf4715daa239291c7ff
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-08-27 11:32:35 +0200

    lab01: added more demo functions

--- lab01/helloworld/src/main/java/com/hvl/dat351/helloworld/HelloworldApplication.java
+++ lab01/helloworld/src/main/java/com/hvl/dat351/helloworld/HelloworldApplication.java
@@ -3,10 +3,14 @@ package com.hvl.dat351.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// Use with localhost:8080/hello?name=Matej
+// Use with:
+//   http://localhost:8080/hello?name=Matej
+//   http://localhost:8080/goodbye?name=Matej
+//   curl -X POST "http://localhost:8080/greet?name=Matej"
 
 @SpringBootApplication
 @RestController
@@ -19,7 +23,17 @@ public class HelloworldApplication {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Hello, " + name + "!";
+        return "Hello, " + name + "!\n";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Goodbye, " + name + "!\n";
+    }
+
+    @PostMapping("/greet")
+    public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Greetings, " + name + "!\n";
     }
 
 }
