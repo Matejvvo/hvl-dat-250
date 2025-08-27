7c3705ac9ac7665fa79321aacbccd6443732b1a7
Matej Volkmer <volkmmat@fit.cvut.cz>
2025-08-27 11:00:05 +0200

    lab01: done

--- lab01/helloworld/src/main/java/com/hvl/dat351/helloworld/HelloworldApplication.java
+++ lab01/helloworld/src/main/java/com/hvl/dat351/helloworld/HelloworldApplication.java
@@ -2,13 +2,24 @@ package com.hvl.dat351.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// Use with localhost:8080/hello?name=Matej
 
 @SpringBootApplication
+@RestController
 public class HelloworldApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         SpringApplication.run(HelloworldApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
     }
 
 }
